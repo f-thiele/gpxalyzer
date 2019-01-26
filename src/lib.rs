008cb6bd383b60ce74d618f8d5ac18f64a3cd939
Fabian Thiele <fabian.thiele@posteo.de>
2019-01-26 13:59:48 +0100

    Include comments for the different functions

--- src/lib.rs
+++ src/lib.rs
@@ -21,26 +21,45 @@ use gpx::{Waypoint, TrackSegment};
 use chrono::{DateTime, Utc};
 
 pub fn distance(a: &Waypoint, b: &Waypoint) -> f64 {
-    // use the haversine formula
+    // calculate the distance between to GPX waypoints by making use of the
+    // haversine formula: distance on two points of a sphere where we know the
+    // angular coordinates of the points [lattitude and longitude]
+
+    // get angles and save them as phi and lambda
     let phi1 = a.point().lat();
     let phi2 = b.point().lat();
     let lambda1 = a.point().lng();
     let lambda2 = b.point().lng();
 
+    // calculate the sinus of half the angular differences
     let sphi = ((phi2-phi1)/2.).sin();
     let slambda = ((lambda2-lambda1)/2.).sin();
 
-    let r = 6.3781*1e6;
+    // radius of the earth
+    let r = 6.3781*1e6; // in meters
 
-    let distance = 2.*r*((sphi.powi(2)+phi1.cos()*phi2.cos()*(slambda.powi(2)))).asin();
-
-    distance
+    // haversine formula
+    return 2.*r*((sphi.powi(2)+phi1.cos()*phi2.cos()*(slambda.powi(2)))).asin();
 }
 
 pub fn decorate_speed(ts: &mut TrackSegment) -> Result<&mut TrackSegment, &'static str> {
+    // decorate the missing speed information on a whole track segment
+    //
+    // TODO: this function needs a lot of work still
+    // right now we are simply calculating the distance between two (reasonably?) distant
+    // waypoints to not trap into precision issues with the GPS information
+    //
+    // better ideas:
+    // a) calculate the speed in every waypoint and average over it
+    // b) or interpolate the track (spline?) and calculate the slope at every point
+
+    // use an interpolation of 15 track points (usually ~1 second apart) to calculate the
+    // distance
     let d_next_wp = 15;
 
     for i in 0..ts.points.len()-d_next_wp-1 {
+        // loop over all waypoints and use d_next_wp for speed
+        // calculation
         let wp = &ts.points[i];
         let nwp = &ts.points[i+d_next_wp];
         let distance = distance(wp, nwp);
@@ -49,14 +68,17 @@ pub fn decorate_speed(ts: &mut TrackSegment) -> Result<&mut TrackSegment, &'stat
             signed_duration_since(wp.time.unwrap())
             .num_seconds() as f64;
         ts.points[i].speed = Some(distance/duration);
+        // TODO: additional error handling of zero divison should be implemented here
 
     }
 
-    return Ok(ts);
+    Ok(ts)
 }
 
 
 pub fn get_range_lattitude(ts: &TrackSegment) -> ([f64; 2]) {
+    // return the lattitude range of the track segment
+
     let mut max_lat = ts.points[0].point().lat();
     let mut min_lat = max_lat;
 
@@ -74,6 +96,8 @@ pub fn get_range_lattitude(ts: &TrackSegment) -> ([f64; 2]) {
 
 
 pub fn get_range_longitude(ts: &TrackSegment) -> ([f64; 2]) {
+    // return the longitude range of the track segment
+
     let mut max_lng = ts.points[0].point().lng();
     let mut min_lng = max_lng;
 
@@ -91,6 +115,8 @@ pub fn get_range_longitude(ts: &TrackSegment) -> ([f64; 2]) {
 
 
 pub fn get_lattitude(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
+    // return a vector of all lattitudes for every waypoint of the track segment
+
     let mut lat = std::vec::Vec::new();
 
     for n in &ts.points {
@@ -102,6 +128,8 @@ pub fn get_lattitude(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
 
 
 pub fn get_longitude(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
+    // return a vector of all longitudes for every waypoint of the track segment
+
     let mut lng = std::vec::Vec::new();
 
     for n in &ts.points {
@@ -113,6 +141,8 @@ pub fn get_longitude(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
 
 
 pub fn get_time(ts: &TrackSegment) -> (std::vec::Vec<DateTime<Utc>>) {
+    // return a vector of all timestamps for every waypoint of the track segment
+
     let mut time = std::vec::Vec::new();
 
     for n in &ts.points {
@@ -124,6 +154,8 @@ pub fn get_time(ts: &TrackSegment) -> (std::vec::Vec<DateTime<Utc>>) {
 
 
 pub fn get_speed(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
+    // return a vector of all speeds for every waypoint of the track segment
+
     let mut speed = std::vec::Vec::new();
 
     for n in &ts.points {
@@ -142,6 +174,8 @@ pub fn get_speed(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
 
 
 pub fn get_elevation(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
+    // return a vector of all elevations for every waypoint of the track segment
+
     let mut elev = std::vec::Vec::new();
 
     for n in &ts.points {
@@ -152,6 +186,7 @@ pub fn get_elevation(ts: &TrackSegment) -> (std::vec::Vec<f64>) {
 }
 
 
+// example test to be written
 #[cfg(test)]
 mod tests {
     #[test]
